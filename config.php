dc6d11a8236845eb93e0377316f7f41b4692e1f7
Ikiscream <33494335+ikii378@users.noreply.github.com>
2024-06-13 18:47:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// config.php
+
+return [
+    [
+        'alias' => 'PersadaYamaha',
+        'cookie' => 'PHPSESSID=a9341143112d15c3efffc22f58623283'
+    ],
+    [
+        'alias' => 'pklo50',
+        'cookie' => 'PHPSESSID=547c2c4616a04de4ccd1ec2daaf0bc58'
+    ],
+    [
+        'alias' => 'Ikiscream',
+        'cookie' => 'PHPSESSID=7856faf6f66b44ae6ea356f73b0d28c4'
+    ]
+];
+?>
