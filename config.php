7c4ef9a23c0976776c05d845b85203d0374e17ec
Ikiscream <33494335+ikii378@users.noreply.github.com>
2024-06-13 18:48:32 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 
 return [
     [
-        'alias' => 'PersadaYamaha',
-        'cookie' => 'PHPSESSID=a9341143112d15c3efffc22f58623283'
+        'alias' => 'Account_1',
+        'cookie' => 'PHPSESSID='
     ],
     [
-        'alias' => 'pklo50',
-        'cookie' => 'PHPSESSID=547c2c4616a04de4ccd1ec2daaf0bc58'
+        'alias' => 'Account_2',
+        'cookie' => 'PHPSESSID='
     ],
     [
-        'alias' => 'Ikiscream',
-        'cookie' => 'PHPSESSID=7856faf6f66b44ae6ea356f73b0d28c4'
+        'alias' => 'Account_3',
+        'cookie' => 'PHPSESSID='
     ]
 ];
 ?>
